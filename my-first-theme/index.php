bafae1c06856636b69b94155f9db5daac90610c6
Slipsager <bjsl@eal.dk>
2016-08-24 11:39:50 +0200

    the post loop
    bloginfo() and the_post() the_content() template tag

---
+++ my-first-theme/index.php
@@ -0,0 +1,18 @@
+<?php
+
+bloginfo('name'); // a template
+
+// the post loop
+if ( have_posts() ) {
+	while ( have_posts() ) {
+		the_post();
+		//
+		// Post Content here
+
+    the_content();  // a template tag that output the post Content
+
+    echo "<hr>";
+
+	} // end while
+} // end if
+?>
